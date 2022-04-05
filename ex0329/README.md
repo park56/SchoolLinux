0fa61ff35c3447a1b1aa843915ec006975442077
park56 <psh021678@gmail.com>
2022-04-05 10:55:32 +0900

    linuxclass

---
+++ ex0329/README.md
@@ -0,0 +1,2 @@
+# SchoolLinux
+2022 1학기 linux 수업
---
+++ ex0329/ex0.1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 100
+
+void copy(char from[], char to[]);
+
+char line[MAXLINE];
+char regstr[MAXLINE];
+
+int main(){
+	int max=0;
+	
+while(1){
+	gets(line);
+	printf("line = %s\n", line);
+
+	int size = strlen(line);
+	printf("size = %d\n",size);
+	if(max < size){
+		max = size;
+		copy(regstr,line);
+	}
+	printf("regstr = %s\n",regstr);
+}
+	return 0;
+
+
+}
+void copy(char from[], char to[]){
+	int i=0;
+	while(from[i] = to[i] != '\0'){
+		i++;
+	}
+
+}
